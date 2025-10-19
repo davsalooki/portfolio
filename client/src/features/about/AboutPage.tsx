d497b0a15ef5eb6938a433ca783388e2410948cb
David Le <davsalooki@gmail.com>
2025-10-19 23:20:27 +1100

    Done About page

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,8 +1,18 @@
 const AboutPage = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">About Us</h1>
-      <p className="mt-4 text-red-600">This is the about page of our application.</p>
+        <h2 className="text-3xl mt-4">David Le</h2>
+        <div className="flex flex-col md:flex-row gap-8 items-start">
+            <div className="space-y-4 break-words my-4 md:w-1/2">
+                <p>Servus, I'm David!</p>
+                <p>I am a science and computer science student at Monash, majoring in pure maths and algorithms.</p>
+                <p>In my free time, I enjoy many hobbies, probably too much. You can talk to me about travelling, motorcycling, hiking, sports, (classical) music, trains, or literally anything. I would probably be interested.</p>
+                <p>Over my studies, I have been fortunate enough to work with a fantastic team at Atlassian.</p>
+            </div>
+            <div className="md:w-1/2">
+                <img src="/me.jpg" alt="David Le" className="w-full h-auto rounded-lg" />
+            </div>
+        </div>
     </div>
   )
 }
