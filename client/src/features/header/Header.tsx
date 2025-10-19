1f68d023818c67d889e646948db9b22af29b2c9c
David Le <davsalooki@gmail.com>
2025-10-19 22:31:06 +1100

    Finished Header

--- client/src/features/header/Header.tsx
+++ client/src/features/header/Header.tsx
@@ -1,25 +1,38 @@
 const Header = () => {
-    return (
-        <div>
-            <nav className="border-b border-gray-300">
-            <a>David Le</a>
-                <ul className="flex space-x-4 p-4 text-xl text-white">
-                    <li>
-                        <a href="/">about</a>
-                    </li>
-                    <li>
-                        <a href="/about">blog</a>
-                    </li>
-                    <li>
-                        <a href="/contact">gallery</a>
-                    </li>
-                    <li>
-                        <a href="/contact">projects</a>
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <div className="mx-4 md:mx-20 lg:mx-70">
+      <nav className="border-b border-gray-300">
+        <div className="flex items-center justify-between p-4">
+          <a href="/" className="text-2xl">
+            David Le
+          </a>
+
+          <ul className="flex space-x-6 text-xl font-extralight">
+            <li>
+              <a href="/" className="hover:text-blue-600">
+                about
+              </a>
+            </li>
+            <li>
+              <a href="/blog" className="hover:text-blue-600">
+                blog
+              </a>
+            </li>
+            <li>
+              <a href="/gallery" className="hover:text-blue-600">
+                gallery
+              </a>
+            </li>
+            <li>
+              <a href="/projects" className="hover:text-blue-600">
+                projects
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </nav>
+    </div>
+  )
 }
 
 export default Header
