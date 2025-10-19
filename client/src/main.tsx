35d1559ecb29b054b420e2dc269fd76fbba48140
David Le <davsalooki@gmail.com>
2025-10-20 00:08:36 +1100

    Routered up pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { Container, createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import AboutPage from './features/about/AboutPage.tsx';
+import BlogPage from './features/blogs/BlogPage.tsx';
+import GalleryPage from './features/gallery/GalleryPage.tsx';
+import Header from './features/header/Header.tsx';
+import Footer from './features/footer/Footer.tsx';
 
-createRoot(document.getElementById('root') as Container).render(
+const root = document.getElementById('root')
+
+createRoot(root as Container).render(
   <StrictMode>
-    <App />
+    <div className="px-4 md:px-7 lg:px-90">
+
+      <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/*" element={<AboutPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/blogs" element={<BlogPage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
+          </Routes>
+          <Footer />
+      </BrowserRouter>
+    </div>
   </StrictMode>,
 )
